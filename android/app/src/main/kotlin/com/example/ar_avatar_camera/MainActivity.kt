b9aac0b7d1e9e68f73e7b51f0c824f003a38d9bc
sen3064 <sensuryo@gmail.com>
2025-11-05 16:53:03 +0700

    .
    Former-commit-id: 7b29b0e0e978f5bc522f28d9a27a5c5435247249
    Former-commit-id: 5d9fe346ff299600616e05976f445d027a3b949b

---
+++ android/app/src/main/kotlin/com/example/ar_avatar_camera/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ar_avatar_camera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/build/app/intermediates/cxx/Debug/5u2z1682/logs/x86_64/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"E:\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\Ristoan Tri Laras\\AppData\\Local\\flutter\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\scripts" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86_64" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86_64" ^
+  "-DANDROID_NDK=E:\\Android\\Sdk\\ndk\\26.3.11579264" ^
+  "-DCMAKE_ANDROID_NDK=E:\\Android\\Sdk\\ndk\\26.3.11579264" ^
+  "-DCMAKE_TOOLCHAIN_FILE=E:\\Android\\Sdk\\ndk\\26.3.11579264\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=E:\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=E:\\Project\\flutter-AR\\android\\build\\app\\intermediates\\cxx\\Debug\\5u2z1682\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=E:\\Project\\flutter-AR\\android\\build\\app\\intermediates\\cxx\\Debug\\5u2z1682\\obj\\x86_64" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BE:\\Project\\flutter-AR\\android\\build\\.cxx\\Debug\\5u2z1682\\x86_64" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli
